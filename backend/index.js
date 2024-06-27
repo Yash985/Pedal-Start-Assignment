36554043c3d627510a5659e5884486f24c9cb7ce
Yash <yashchandra985@gmail.com>
2024-06-27 16:42:18 +0530

    Initialized backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+//Depencencies imports
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
